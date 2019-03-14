1a04bd640c12b092833e1fa6890c9023f7ef6458
Kyle Plamondon <kyle.plamondon@gmail.com>
2019-03-14 16:29:31 -0700

    First commit

---
+++ Project_Test1.m
@@ -0,0 +1,14 @@
+%% Clear
+clc;
+clear;
+%% Load the image
+B1 = im2double(imread('Image_Frame118.jpg'));
+B2 = im2double(imread('Image_Frame119.jpg'));
+B3 = im2double(imread('Image_Frame120.jpg'));
+B_avg = (B1 + B2 + B3) ./ 3;
+
+I = im2double(imread('Image_Frame121.jpg'));
+I_subtr = I - B_avg;
+
+imshow(B_avg);
+imshow(I_subtr);
