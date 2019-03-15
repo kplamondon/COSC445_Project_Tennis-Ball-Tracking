b8722c1803e652ab224f6198315f8a44097bf3e9
Kyle Plamondon <kyle.plamondon@gmail.com>
2019-03-14 18:07:48 -0700

    Changed directory to image in matlab file

--- Project_Test1.m
+++ Project_Test1.m
@@ -2,12 +2,12 @@
 clc;
 clear;
 %% Load the image
-B1 = im2double(imread('Image_Frame118.jpg'));
-B2 = im2double(imread('Image_Frame119.jpg'));
-B3 = im2double(imread('Image_Frame120.jpg'));
+B1 = im2double(imread('assets/video_1/Image_Frame118.jpg'));
+B2 = im2double(imread('assets/video_1/Image_Frame119.jpg'));
+B3 = im2double(imread('assets/video_1/Image_Frame120.jpg'));
 B_avg = (B1 + B2 + B3) ./ 3;
 
-I = im2double(imread('Image_Frame121.jpg'));
+I = im2double(imread('assets/video_1/Image_Frame121.jpg'));
 I_subtr = I - B_avg;
 
 imshow(B_avg);
