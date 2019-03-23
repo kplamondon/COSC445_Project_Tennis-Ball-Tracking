9c99442f18a78cacf0e4111e248e9a838594a002
Kyle Plamondon <kyle.plamondon@gmail.com>
2019-03-23 13:40:08 -0700

    Trying other video

--- Project_Test2_GausBackSubtr.m
+++ Project_Test2_GausBackSubtr.m
@@ -3,7 +3,7 @@ clc;
 clear;
 %% Step 1 - Import Video and Initialize Foreground Detector
 FG_Detector = vision.ForegroundDetector('NumGaussians',5,'NumTrainingFrames', 15);
-videoReader = vision.VideoFileReader('assets/video_1/video1.mp4');
+videoReader = vision.VideoFileReader('assets/video_2/video2.mp4');
 %% Step 2 - Detect Cars
 videoPlayer = vision.VideoPlayer('Name', 'Gausian Background Subtraction');
 videoPlayer.Position(3:4) = [960,640]; % window size: [width, height]
@@ -14,18 +14,18 @@ se = strel('disk', 2); % morphological filter for noise removal
 while ~isDone(videoReader)
     frame = step(videoReader); % read the next video frame
     % Colour segment
-    [BW,frame_rgbSegmented] = createMask3(frame);
+    %[BW,frame_rgbSegmented] = createMask3(frame);
     % Detect the FG in the current video frame
-    FG = step(FG_Detector, frame_rgbSegmented);
+    FG = step(FG_Detector, frame);
     % Use morphological opening to remove noise in the FG
     filteredFG = imopen(FG, se);
     % Detect connected components with specified max area & find bounding boxes
     bbox = step(blobAnalysis, filteredFG);
     % Draw bounding boxes around the detected cars
-    result = insertShape(frame_rgbSegmented, 'Rectangle', bbox, 'Color', 'green');
+    %result = insertShape(frame_rgbSegmented, 'Rectangle', bbox, 'Color', 'green');
     % Display the number of cars found in the video frame
     numBalls = size(bbox, 1);
     %result = insertText(result, [10 10], numBalls, 'BoxOpacity', 1, 'FontSize', 14);
-    step(videoPlayer, result); % display the results
+    step(videoPlayer, FG); % display the results
 end
 release(videoPlayer); % Close the video file
