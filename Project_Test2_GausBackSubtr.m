04513dfa5f2b098bef714adb662c79614c832cd1
Kyle Plamondon <kyle.plamondon@gmail.com>
2019-03-23 22:22:56 -0700

    Some small changes since last

--- Project_Test2_GausBackSubtr.m
+++ Project_Test2_GausBackSubtr.m
@@ -4,13 +4,14 @@ clear;
 %% Step 1 - Import Video and Initialize Foreground Detector
 FG_Detector = vision.ForegroundDetector('NumGaussians',5,'NumTrainingFrames', 5);
 videoReader = vision.VideoFileReader('assets/video_2/video2.mp4');
-%% Step 2 - Detect Cars
+%% Step 2 - Detect tenis ball candidates
 videoPlayer = vision.VideoPlayer('Name', 'Gausian Background Subtraction');
 videoPlayer.Position(3:4) = [960,640]; % window size: [width, height]
 blobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
     'AreaOutputPort', false, 'CentroidOutputPort', false, ...
-    'MaximumBlobArea', 300, 'MinimumBlobArea', 5);
-se = strel('disk', 2); % morphological filter for noise removal
+    'MaximumBlobArea', 300, 'MinimumBlobArea', 6);
+se = strel('disk', 3); % morphological filter for noise removal
+peopleDetector = vision.PeopleDetector; %used for HOG detection of players
 while ~isDone(videoReader)
     frame = step(videoReader); % read the next video frame
     % Detect the FG in the current video frame using Gausian
@@ -24,17 +25,31 @@ while ~isDone(videoReader)
     FG = bitand(FG_gaus, FG_segment);
     
     % Use morphological opening to remove noise in the FG
-    filteredFG = imopen(FG, se);
+    filteredFG = imopen(FG, se); 
     
+    % Now we need to remove the false candidates from the player
+    % -> this is done by first detecting the player with HOG
+    %[bboxPeople, peopleScores] = peopleDetector(frame); 
+    %Using the bbox of the people we remove objects inside the bbox
+    %for i=1:length(bboxPeople)
+    %   filteredFG(bboxPeople) = 0; 
+    %end
     
+    % Now we need to detect the lines of the court and remove these as well
+    %E = edge(rgb2gray(frame), 'canny',[],5);
+    %[H,T,R] = hough(E);
+    %peaks = houghpeaks(H,50,'Threshold',30);
+    %lines = houghlines(E,T,R,peaks,'FillGap',5,'MinLength',50);
     
     % Detect connected components with specified max area & find bounding boxes
-    %bbox = step(blobAnalysis, filteredFG);
+    bbox = step(blobAnalysis, filteredFG);
     % Draw bounding boxes around the detected objects
-    %result = insertShape(frame, 'Rectangle', bbox, 'Color', 'green');
+    result = insertShape(frame, 'Rectangle', bbox, 'Color', 'green');
+    %result = insertObjectAnnotation(result,'rectangle',bboxPeople,peopleScores);
+    %result = insertShape(result, 'Line', [lines.point1 lines.point2], 'Color', 'cyan');
     % Display the number of tennis balls found in the video frame
-    %numBalls = size(bbox, 1);
-    %result = insertText(result, [10 10], numBalls, 'BoxOpacity', 1, 'FontSize', 14);
-    step(videoPlayer, FG); % display the results
+    numBalls = size(bbox, 1);
+    result = insertText(result, [10 10], numBalls, 'BoxOpacity', 1, 'FontSize', 14);
+    step(videoPlayer, result); % display the results
 end
 release(videoPlayer); % Close the video file
